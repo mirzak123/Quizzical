130dc26ab868e828b7574f7da5c35d9ba47fb31d
Mirza Karic <mirza1karic1@gmail.com>
2023-03-20 22:03:31 +0100

    Added categories and difficulty

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState, useEffect } from 'react'
 import Question from './Components/Question'
+import SetupMenu from './Components/SetupMenu'
 import { nanoid } from 'nanoid'
 import "./App.css"
 
 function App() {
   const [questions, setQuestions] = useState([])
   const [showAnswers, setShowAnswers] = useState(false)
+  const [showSetup, setShowSetup] = useState(true)
+  const [questionParameters, setQuestionParameters] = useState({
+    category: "",
+    difficulty: "easy"
+  })
 
   async function getNewQuestions() {
-    const triviaUrl = 'https://opentdb.com/api.php?amount=5'
+    const triviaUrl = "https://opentdb.com/api.php?amount=5"
+      // if there is a set value for difficulty and category add it to the url
+      + `${questionParameters.category ? `&category=${questionParameters.category}` : ''}`
+      + `${questionParameters.difficulty ? `&difficulty=${questionParameters.difficulty}` : ''}`
+    console.log(triviaUrl)
     const response = await fetch(triviaUrl)
     const data = await response.json()
 
@@ -41,40 +51,14 @@ function App() {
   }
   
   useEffect(() => {
-    // const triviaUrl = 'https://opentdb.com/api.php?amount=5'
-    // fetch(triviaUrl)
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     const questionObjects = data.results.map(item => {
-    //       // create answer objects from the incorrect answers
-    //       const answers = [...item['incorrect_answers']].map(answer => ({
-    //         answer: answer,
-    //         isCorrect: false,
-    //         isSelected: false
-    //       }))
-    //
-    //       // add the correct answer to the answers array
-    //       answers.push({
-    //         answer: item['correct_answer'],
-    //         isCorrect: true,
-    //         isSelected: false
-    //       })
-    //
-    //       // shuffle the answers
-    //       answers.sort(() => Math.random() - 0.5)
-    //
-    //       return {
-    //         id: nanoid(),
-    //         question: item.question,
-    //         answers: answers
-    //       }
-    //     })
-    //
-    //     setQuestions(questionObjects)
-    //   })
-
     getNewQuestions().then(data => setQuestions(data))
   }, [])
+
+  function startGame() {
+    getNewQuestions().then(data => setQuestions(data))
+    setShowAnswers(false)
+    setShowSetup(false)
+  }
 
   function toggleAnswer(questionId, answer) {
     setQuestions(prevQuestions => prevQuestions.map(questionObject => {
@@ -121,31 +105,59 @@ function App() {
     />
   ))
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setQuestionParameters(prevParameters => {
+      return {
+        ...prevParameters,
+        [name]: value
+      }
+    })
+  }
+
   return (
     <main className="quizzical">
-      <div className="quizzical-container">
-        { questionElements }
-      </div>
-      <div className="button-container">
-        {
-          showAnswers ?
-            <>
-              <h2>You scored {getCorrectAnswers()}/5 correct answers</h2>
-              <button
-                className="button"
-                onClick={resetGame}
-              >
-                Play again
-              </button>
-            </> :
-            <button
-              className="button"
-              onClick={checkAnswers}
-            >
-              Check answers
-            </button>
+      {
+        showSetup ?
+          <SetupMenu
+            startGame={startGame}
+            questionParameters={questionParameters}
+            handleChange={handleChange}
+          /> :
+          <div>
+            <div className="quizzical-container">
+              { questionElements }
+            </div>
+            <div className="quizzical-container__controller">
+              {
+                showAnswers ?
+                  <>
+                    <h2 className="score">You scored {getCorrectAnswers()}/5 correct answers</h2>
+                    <div className="button-container">
+                      <button
+                        className="button button--inverted"
+                        onClick={() => setShowSetup(true)}
+                      >
+                        Restart
+                      </button>
+                      <button
+                        className="button"
+                        onClick={resetGame}
+                      >
+                        Play again
+                      </button>
+                    </div>
+                  </> :
+                  <button
+                    className="button"
+                    onClick={checkAnswers}
+                  >
+                    Check answers
+                  </button>
+              }
+            </div>
+          </div>
       }
-      </div>
     </main>
   )
 }
---
+++ src/Components/SetupMenu.jsx
@@ -0,0 +1,52 @@
+export default function SetupMenu(props) {
+  return (
+    <div
+      className="setup-container"
+    >
+      <h1>Quizzical</h1>
+      <div className="question-configuration">
+        <h2>Question preferences</h2>
+        <div className="preference-wrapper">
+          <label htmlFor="category">Category</label>
+          <select
+            onChange={props.handleChange}
+            className="select-category"
+            name="category"
+            id="category"
+            value={props.questionParameters.category}
+          >
+            <option value="">General knowledge</option>
+            <option value="18">Computers</option>
+            <option value="19">Mathematics</option>
+            <option value="11">Film</option>
+            <option value="10">Books</option>
+            <option value="15">Video Games</option>
+            <option value="21">Sports</option>
+            <option value="23">History</option>
+          </select>
+        </div>
+        <div className="preference-wrapper">
+          <label htmlFor="difficulty">Difficulty</label>
+          <select
+            onChange={props.handleChange}
+            className="select-difficulty"
+            name="difficulty"
+            id="difficulty"
+            value={props.questionParameters.difficulty}
+          >
+            <option value="">Any</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+        </div>
+      </div>
+      <button
+        className="button"
+        onClick={props.startGame}
+      >
+        Start quiz
+      </button>
+    </div>
+  )
+}
